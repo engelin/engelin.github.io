74a7a77c169ad9d7da6f380e785b365716198511
Yerin <yerinhong0927@gmail.com>
2017-07-19 16:51:33 +0900

    Create 2017-7-19-Tegra-multimedia-api-build.m

---
+++ _posts/2017-7-19-Tegra-multimedia-api-build.m
@@ -0,0 +1,8 @@
+## Building Tegra Multimedia API
+
+Included in Jetpack 3.0.
+The Jetson Multimedia API package provides low level APIs for flexible application development.
+
+![Folder](https://github.com/engelin/engelin.github.io/blob/master/images/Tegra_Multimedia_API_1.png?raw=true)
+
+### Build argus

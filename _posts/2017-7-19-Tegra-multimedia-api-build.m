f9f2a9136f2662ef75d185062cd9702af4e7b5bb
Yerin <yerinhong0927@gmail.com>
2017-07-19 16:59:04 +0900

    Update 2017-7-19-Tegra-multimedia-api-build.m

--- _posts/2017-7-19-Tegra-multimedia-api-build.m
+++ _posts/2017-7-19-Tegra-multimedia-api-build.m
@@ -6,3 +6,24 @@ The Jetson Multimedia API package provides low level APIs for flexible applicati
 ![Folder](https://github.com/engelin/engelin.github.io/blob/master/images/Tegra_Multimedia_API_1.png?raw=true)
 
 ### Build argus
+
+Install gtk-3.0
+```markdown
+$ sudo apt-get install libgtk-3-dev
+```
+
+Build argus
+```markdown
+$ cd tegra_multimedia_api/argus
+$ mkdir build
+$ cd build
+$ cmake ../
+$ make
+$ sudo make install
+```
+
+### Build tegra_multimedia_api folder
+```markdown
+$ cd tegra_multimedia_api
+$ make
+```
